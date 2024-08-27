67888c53a420afb48ead560bf45fb8fab75aafd0
Nombuyiselo Msweli <mswelinombuyiselo@gmail.com>
2024-08-27 14:43:06 +0200

    updated index.html with new id for paragraph, moved script to end of body and updated script.js with validation function to display valid or invalid statement. To do: ensure validation checks p_ + numbers

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 function validateSyntax() {
     let input = document.getElementById('petInput').value;
     // Validation logic goes here
-    let result = ''; // Placeholder for validation result
+    let result = " "; // Placeholder for validation result
 
     // TODO: Write your validation logic here
     console.log(input)
         // Check if input starts with 'pet_' and followed by alphanumeric characters
+            let resultEl = document.getElementById("resultEl")
 
-            document.getElementById('result').innerText = result;
+            
+            //I need to get input to be displayed in a paragraph
+            if ( input === "p_") {
+                result = "Valid Syntax 🟢"
+            }
+            else {
+                result = "Invalid Syntax 🔴"
+            }
+
+            resultEl.textContent = result
+
+           //document.getElementById('result').innerText = result;
 }
 
 
