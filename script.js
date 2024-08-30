f3a921949536103d3fb9e4fbdcbc366be830a512
Nombuyiselo Msweli <mswelinombuyiselo@gmail.com>
2024-08-30 18:35:40 +0200

    added if condition statement and message valid or invalid to display

--- script.js
+++ script.js
@@ -7,12 +7,21 @@ function validateSyntax() {
     console.log(input)
         // Check if input starts with 'pet_' and followed by alphanumeric characters
         
-        document.getElementById('result').innerText = result;
+        //document.getElementById('result').innerText = result;
+        let resultEl = document.getElementById("resultEl");
 
             let regex = /^(?=.*[pet])(?=.*_)(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z])(?!.*\W)(?!.* ).{8,16}$/ ;                                           
             //password pattern = password must conatin at least the letters 'pet', one underscore, min 1 number, min 1 capital letter, min 1 lowercase letter, no special characters amd no spaces
-
             console.log(input, regex.test(input) )
+ 
+            if (input === regex) {
+                message = "Valid Syntax 🟢"
+            }
+            else {
+                message = "Invalid Syntax 🔴"
+            }
+
+            resultEl.textContent = message 
             
 }
 
